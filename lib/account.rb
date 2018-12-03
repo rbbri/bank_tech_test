308bdf0d0485928bf97d2b7677e375d28c2f8362
rbbri <robin.s.johnson@gmail.com>
2018-12-03 12:45:41 +0000

    Add deposit
    Allows a client to deposit an amount to their account which increases 
    account balance. Includes a minimum deposit guard to prevent using 
    deposit to withdraw from an account 
    
    Refactors test for not being able to use the balance method to change 
    balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,16 @@
 # Account interface for client
 class Account
+  MIN_DEPOSIT = 1
 
   def initialize
     @balance = 0
   end
 
-  def balance
-    @balance
+  attr_reader :balance
+
+  def deposit(amount)
+    raise "Minimum deposit is #{MIN_DEPOSIT}" if amount < MIN_DEPOSIT
+
+    @balance += amount
   end
-  
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,9 +14,17 @@ describe 'Account' do
       expect(account.balance).to equal 0
     end
     it 'should not let a client change the balance' do
-      account.balance + 100
-      expect(account.balance).to equal 0
+      expect{ account.balance = 100 }.to raise_error NoMethodError
     end
   end
 
+  describe '#deposit' do
+    it 'should add an amount to the account' do
+      expect { account.deposit(1) }.to change { account.balance }.from(0).to(1)
+    end
+    it 'should only accept deposits over a a minimum amount' do
+      min = Account::MIN_DEPOSIT
+      expect { account.deposit(-1) }.to raise_error "Minimum deposit is #{min}"
+    end
+  end
 end
