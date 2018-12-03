b91886842bcdda5d5801fad37333633f13081da8
rbbri <robin.s.johnson@gmail.com>
2018-12-03 13:42:42 +0000

    Add withdrawal
    Allows a client to withdraw an amount from an account which decreases 
    the account balance. Includes guards for minimum and maximum withdrawal
    
    Reinstates attr_reader for balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,11 @@
 # Account interface for client
 class Account
-  MIN_DEPOSIT = 1
+  MIN_BALANCE = 0
+  MIN_DENOMINATION = 0.01
+  MIN_DEPOSIT = 0.01
+  MIN_WITHDRAWAL = 0.01
+
+
 
   def initialize
     @balance = 0
@@ -10,7 +15,17 @@ class Account
 
   def deposit(amount)
     raise "Minimum deposit is #{MIN_DEPOSIT}" if amount < MIN_DEPOSIT
-
     @balance += amount
+  end
+
+  def withdraw(amount)
+    raise "Maximum withdrawal is #{max_withdrawal}" if amount > max_withdrawal
+    @balance -= amount
+  end
+
+  private
+
+  def max_withdrawal
+    @balance - MIN_BALANCE
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,10 @@ require 'account'
 
 describe 'Account' do
   subject(:account) { Account.new }
+  let(:min_denomination) { Account::MIN_DENOMINATION }
+  let(:min_deposit) { Account::MIN_DEPOSIT }
+  let(:min_withdrawal) { Account::MIN_WITHDRAWAL }
+
 
   describe '#initialize' do
     it 'should create an account with a balance of zero' do
@@ -10,21 +14,45 @@ describe 'Account' do
   end
 
   describe '#balance' do
-    it 'should return the balance' do
+    it 'returns the balance' do
       expect(account.balance).to equal 0
     end
-    it 'should not let a client change the balance' do
-      expect{ account.balance = 100 }.to raise_error NoMethodError
+    it 'raises an error if a client tries to set the balance' do
+      expect { account.balance += 100 }
+        .to raise_error(NoMethodError)
     end
   end
 
   describe '#deposit' do
-    it 'should add an amount to the account' do
-      expect { account.deposit(1) }.to change { account.balance }.from(0).to(1)
+    it 'adds an amount to the account' do
+      expect { account.deposit(1) }
+        .to change { account.balance }.from(0).to(1)
     end
-    it 'should only accept deposits over a a minimum amount' do
-      min = Account::MIN_DEPOSIT
-      expect { account.deposit(-1) }.to raise_error "Minimum deposit is #{min}"
+    it 'raises an error if the deposit is under a minimum amount' do
+      invalid_deposit = min_deposit - min_denomination
+      expect { account.deposit(invalid_deposit) }
+        .to raise_error("Minimum deposit is #{min_deposit}")
+    end
+  end
+
+  describe '#withdraw' do
+    before do
+      account.deposit(min_deposit)
+    end
+    it 'subtracts an amount from account' do
+      expect { account.withdraw(min_denomination) }
+        .to change { account.balance }.from(0.01).to(0)
+    end
+    it 'raises an error if the withdrawal is under a minimum amount' do
+      invalid_withdrawal = min_withdrawal - min_denomination
+      expect { account.deposit(invalid_withdrawal) }
+        .to raise_error("Minimum deposit is #{min_deposit}")
+    end
+    it 'raises an error if withdrawal exceeds available funds' do
+      max = account.balance
+      invalid_withdrawal = max + min_denomination
+      expect { account.withdraw(invalid_withdrawal) }
+        .to raise_error "Maximum withdrawal is #{max}"
     end
   end
 end
